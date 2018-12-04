92c5f26038ead902a65de1e0df39f0ba5c2d35dc
Daniel Almeida <34009600+danielalmeida1481@users.noreply.github.com>
2018-12-04 19:12:17 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const { Client, Attachment } = require('discord.js');
+
+const client = new Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === '!cantina') {
+        // Create the attachment using Attachment
+        const attachment = new Attachment('https://i1.wp.com/www.ondevamosjantar.com/wp-content/uploads/2015/05/cantina-lx-lx-factory-alcantara-lisboa-restaurante-grupos.jpg?resize=614%2C312');
+        // Send the attachment in the message channel with a content
+        message.channel.send(`${message.author},`, attachment);
+    }
+});
+
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login(process.env.TOKEN);
