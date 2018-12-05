0371ad8abfc076214faf5848cdd6a24eb3e645e5
Daniel Almeida <34009600+danielalmeida1481@users.noreply.github.com>
2018-12-05 17:21:59 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -9,9 +9,17 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === '!cantina') {
         // Create the attachment using Attachment
-        const attachment = new Attachment('https://i.imgur.com/TKHOATK.jpg');
+        const attachment = new Attachment('https://i1.wp.com/www.ondevamosjantar.com/wp-content/uploads/2015/05/cantina-lx-lx-factory-alcantara-lisboa-restaurante-grupos.jpg?resize=614%2C312');
         // Send the attachment in the message channel with a content
         message.channel.send(`${message.author},`, attachment);
+    } else if (message.content === '!random') {
+        fetch('http://quotes.stormconsultancy.co.uk/random.json', {
+            method: 'get'
+        }).then(function(response) {
+            message.channel.send(`${message.author}, ${response.quote}`);
+        }).catch(function(err) {
+            
+        });
     }
 });
 
